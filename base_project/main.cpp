7dc370f45b071c8d1d9e06af17c2347010514fd4
AlexisMarasigan <alexismarasigan31@gmail.com>
2022-06-05 18:21:21 +0200

    update

--- base_project/main.cpp
+++ base_project/main.cpp
@@ -2,82 +2,73 @@
  * @file main.cpp
  * @brief esempi di utilizzo delle librerie del progetto
  * @date 2022-05-31
- * 
+ *
  */
 
-#include<iostream>
+#include <iostream>
 #include "board.h"
 #include "move.h"
 #include "game.h"
 #include "player.h"
 
-int main() {
+int main()
+{
     /// Come inizializzare un oggetto di tipo Piece
     board::Piece piece = board::Piece::BLUE_DISC;
-    board::HexBoard board(11); // Crea una board vuota 11x11
-    board::HexBoard empty_board; // Crea una board vuota 0x0
+    board::HexBoard board(11);         // Crea una board vuota 11x11
+    board::HexBoard empty_board;       // Crea una board vuota 0x0
     board::HexBoard copy_board(board); // Crea una board copiandone un'altra
-    int size = board.getSize(); // Ottengo la dimensione di una board
-    std::pair<int,int> p = std::make_pair(1, 2); 
-    int p1 = p.first; // Prima coordinata, 1
-    int p2 = p.second; // Seconda coordinate, 2
-    bool x = board.isValidPos(p); // true
-    board::Piece piece_ij = board.getPieceAtPos(std::make_pair(3,4)); // Pezzo in posizione (3,4)
-    board.addPiece(piece, std::make_pair(5,6));
-    board.addPiece(piece, std::make_pair(6,6));
-    std::vector<std::pair<int,int>> blue_pos = board.getPosByPiece(piece); // Tutte le posizioni con pedine blu
+    int size = board.getSize();        // Ottengo la dimensione di una board
+    std::pair<int, int> p = std::make_pair(1, 2);
+    int p1 = p.first;                                                  // Prima coordinata, 1
+    int p2 = p.second;                                                 // Seconda coordinate, 2
+    bool x = board.isValidPos(p);                                      // true
+    board::Piece piece_ij = board.getPieceAtPos(std::make_pair(3, 4)); // Pezzo in posizione (3,4)
+    board.addPiece(piece, std::make_pair(5, 6));
+    board.addPiece(piece, std::make_pair(6, 6));
+    std::vector<std::pair<int, int>> blue_pos = board.getPosByPiece(piece); // Tutte le posizioni con pedine blu
     std::cout << "La scacchiera contiene " << blue_pos.size() << " pedine blu.\n";
-    std::vector<std::pair<int,int>> adj_pos = board.getAdjacentPos(std::make_pair(10,0)); // Mostra le posizioni adiacenti
+    std::vector<std::pair<int, int>> adj_pos = board.getAdjacentPos(std::make_pair(10, 0)); // Mostra le posizioni adiacenti
     std::cout << "La scacchiera contiene " << adj_pos.size() << " posizioni adiacenti a (10,0).\n";
     board::HexBoard small_board(3); // Creo una piccola scacchiera 3x3 e la riempio
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(0,0));
-    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(0,1));
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(0,2));
-    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(1,0));
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(1,1));
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(1,2));
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(2,0));
-    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(2,1));
-    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(2,2));
-    if (small_board.checkWin(1))    // Cerco di capire chi ha vinto
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(0, 0));
+    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(0, 1));
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(0, 2));
+    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(1, 0));
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(1, 1));
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(1, 2));
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(2, 0));
+    small_board.addPiece(board::Piece::BLUE_DISC, std::make_pair(2, 1));
+    small_board.addPiece(board::Piece::RED_DISC, std::make_pair(2, 2));
+    if (small_board.checkWin(1)) // Cerco di capire chi ha vinto
         std::cout << "Vince il Rosso!\n";
     else
         std::cout << "Vince il Blu!\n";
     /// Esempi di utilizzo di Action e Move
-    move::Action a_add(move::ActionKind::ADD, piece, p); // Creazione dell'azione ADD
-    move::Move m_add(move::MoveKind::ADD, a_add); // Creazione della mossa ADD
-    move::MoveKind m_kind = m_add.kind; // Tipo della mossa (move::MoveKind::ADD)
-    std::pair<int,int> position_add = m_add.action.position; // Posizione della mossa
-    move::Action a_add_2(move::ActionKind::ADD, board::Piece::BLUE_DISC, std::make_pair(1,2)); // Creo un'azione equivalente alla prima
-    if (a_add==a_add_2) std::cout << "Azioni equivalenti\n"; // Verifico che le due azioni siano equivalenti
+    move::Action a_add(move::ActionKind::ADD, piece, p);                                        // Creazione dell'azione ADD
+    move::Move m_add(move::MoveKind::ADD, a_add);                                               // Creazione della mossa ADD
+    move::MoveKind m_kind = m_add.kind;                                                         // Tipo della mossa (move::MoveKind::ADD)
+    std::pair<int, int> position_add = m_add.action.position;                                   // Posizione della mossa
+    move::Action a_add_2(move::ActionKind::ADD, board::Piece::BLUE_DISC, std::make_pair(1, 2)); // Creo un'azione equivalente alla prima
+    if (a_add == a_add_2)
+        std::cout << "Azioni equivalenti\n"; // Verifico che le due azioni siano equivalenti
     /// Esempi di utilizzo di oggetti Situation per configurazioni di gioco
-    board::HexBoard new_board(11); // Creo una nuova board vuota 11x11
+    board::HexBoard new_board(11);                                     // Creo una nuova board vuota 11x11
     new_board.addPiece(board::Piece::RED_DISC, m_add.action.position); // Simulo la prima mossa (Rosso) sulla board
-    game::Situation situation(new_board, 2); // Creo una Situation, turn=2 (tocca al blu)
-    if (situation.getPieceAtPos(p)==board::Piece::RED_DISC) // Verifico che ci sia una pedina rossa in posizione p
+    game::Situation situation(new_board, 2);                           // Creo una Situation, turn=2 (tocca al blu)
+    if (situation.getPieceAtPos(p) == board::Piece::RED_DISC)          // Verifico che ci sia una pedina rossa in posizione p
         std::cout << "Pedina rossa\n";
     if (situation.pieRule()) // Verifico che sia possibile utilizzare la pie rule
         std::cout << "Pie rule utilizzabile\n";
-    if (situation.getPieceByTurn()==board::Piece::BLUE_DISC) // Ottengo il pezzo utilizzabile dal giocatore al turno corrente
+    if (situation.getPieceByTurn() == board::Piece::BLUE_DISC) // Ottengo il pezzo utilizzabile dal giocatore al turno corrente
         std::cout << "Tocca al blu\n";
     if (!situation.isValid(m_add)) // Verifico che non posso ripetere la stessa mossa
         std::cout << "Mossa non valida.\n";
-    std::vector<move::Move> mosse_valide = situation.validMoves(); // Ottengo il vettore delle mosse valide in questa configurazione
-    std::cout << "Mosse valide: " << mosse_valide.size() << std::endl; // Numero di mosse valide: 120 ADD + 1 SWAP + 1 RESIGN
+    std::vector<move::Move> mosse_valide = situation.validMoves();      // Ottengo il vettore delle mosse valide in questa configurazione
+    std::cout << "Mosse valide: " << mosse_valide.size() << std::endl;  // Numero di mosse valide: 120 ADD + 1 SWAP + 1 RESIGN
     game::Situation new_situation = situation.next(mosse_valide.at(5)); // Applico una mossa valida per ottenere una nuova situazione
-    if (new_situation.turn>0) // Verifico che il gioco non sia già terminato
+    if (new_situation.turn > 0)                                         // Verifico che il gioco non sia già terminato
         std::cout << "Il gioco va avanti.\n";
 
-
-
-
-
-
-
-
-
-
-
-
-    //player::Player p;
+    // player::Player p;
 }
--- base_project/player.h
+++ base_project/player.h
@@ -1,16 +1,17 @@
-#ifndef PLAYER_H 
+#ifndef PLAYER_H
 #define PLAYER_H
 
-#include<string>
+#include <string>
 #include "board.h"
 #include "move.h"
 #include "game.h"
 
-/** In questo namespace potete aggiungere tutte le classi e le funzioni di 
- *  cui avete bisogno, ma non potete in alcun modo modificare 
+/** In questo namespace potete aggiungere tutte le classi e le funzioni di
+ *  cui avete bisogno, ma non potete in alcun modo modificare
  *  le funzioni già esistenti.
  */
-namespace player {
+namespace player
+{
     /**
      * CLASSE DA COMPLETARE PER IL PROGETTO.
      * Classe che implementa il giocatore di Hex.
@@ -20,15 +21,17 @@ namespace player {
      * che riceve una situazione di gioco e ritorna in output
      * la mossa scelta dal giocatore in tale situazione.
      */
-    class Player {
-        public:
+    class Player
+    {
+    public:
         /**
          * @brief Restituisce  il nome della squadra
          * INSERIRE IL NOME!
          * @return std::string nome della squadra
          */
-        std::string getName() {
-            std::string player_name="";
+        std::string getName()
+        {
+            std::string player_name = "";
             return player_name;
         }
 
@@ -37,23 +40,25 @@ namespace player {
          * INSERIRE LE MATRICOLE!
          * @return std::string matricole separate da uno spazio
          */
-        std::string getIDs() {
-            std::string matricola_1="";
-            std::string matricola_2="";
+        std::string getIDs()
+        {
+            std::string matricola_1 = "1950540";
+            std::string matricola_2 = "";
             return matricola_1 + " " + matricola_2;
         }
 
         /**
          * FUNZIONE DA IMPLEMENTARE PER IL PROGETTO. NON cambiare
          * l'intestazione, ma scrivere solo nel corpo della funzione.
-         * 
+         *
          * Questa funzione riceve in input una configurazione o situazione
          * di gioco e restituisce in output la mossa scelta dal giocatore.
-         * 
+         *
          * @param situation configurazione o situazione di gioco
          * @return move::Move mossa scelta dal giocatore
          */
-        move::Move getMoveFromSit(game::Situation situation) {
+        move::Move getMoveFromSit(game::Situation situation)
+        {
             move::Move m; // Si noti che una mossa inizializzata in questo modo è di tipo RESIGN (mossa vuota)
             return m;
         }
